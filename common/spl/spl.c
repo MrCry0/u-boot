igoropaniuk/u-boot-1

[FIO internal] spl: enable hw_watchdog for non-dm case
Call hw_watchdog_init() for the case when SPL watchdog support is
enabled, however without DM support (CONFIG_WDT).

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

common/spl/spl.c
@@ -669,8 +669,12 @@ void board_init_r(gd_t *dummy1, ulong dummy2)
 	spl_board_init();
 #endif
 
-#if defined(CONFIG_SPL_WATCHDOG_SUPPORT) && CONFIG_IS_ENABLED(WDT)
+#if defined(CONFIG_SPL_WATCHDOG_SUPPORT)
+#if CONFIG_IS_ENABLED(WDT)
 	initr_watchdog();
+#else /* SPL_HW_WATCHDOG */
+	hw_watchdog_init();
+#endif
 #endif
 
 	if (IS_ENABLED(CONFIG_SPL_OS_BOOT) || CONFIG_IS_ENABLED(HANDOFF) ||

MrCry0/u-boot

Co-developed-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

common/spl/spl.c
@@ -36,6 +36,7 @@
 #include <fdt_support.h>
 #include <bootcount.h>
 #include <wdt.h>
+#include <watchdog.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -747,8 +748,14 @@ void board_init_r(gd_t *dummy1, ulong dummy2)
 	spl_board_init();
 #endif
 
-#if defined(CONFIG_SPL_WATCHDOG) && CONFIG_IS_ENABLED(WDT)
+#if defined(CONFIG_SPL_WATCHDOG)
+#if CONFIG_IS_ENABLED(WDT)
 	initr_watchdog();
+#elif CONFIG_IS_ENABLED(HW_WATCHDOG)
+	hw_watchdog_init();
+#else
+# error	Either SPL_HW_WATCHDOG or SPL_WDT should be enabled.
+#endif
 #endif
 
 	if (IS_ENABLED(CONFIG_SPL_OS_BOOT) || CONFIG_IS_ENABLED(HANDOFF) ||