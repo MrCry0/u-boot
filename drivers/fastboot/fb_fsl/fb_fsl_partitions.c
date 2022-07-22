igoropaniuk/u-boot-1

[FIO internal] fastboot: fix length calculation for imx-boot image
Fix length calculation for imx-boot image, take into account additional
offset to IVT.

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

drivers/fastboot/fb_fsl/fb_fsl_partitions.c
@@ -254,7 +254,7 @@ static int _fastboot_parts_load_from_ptable(void)
 				/ dev_desc->blksz;
 #else
 	ptable[PTN_BOOTLOADER_INDEX].length =
-				 ANDROID_BOOTLOADER_SIZE / dev_desc->blksz;
+				(ANDROID_BOOTLOADER_SIZE - bootloader_mmc_offset()) / dev_desc->blksz;
 #endif
 	ptable[PTN_BOOTLOADER_INDEX].partition_id = boot_partition;
 	ptable[PTN_BOOTLOADER_INDEX].flags = FASTBOOT_PTENTRY_FLAGS_UNERASEABLE;

MrCry0/u-boot

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/fastboot/fb_fsl/fb_fsl_partitions.c
@@ -258,7 +258,7 @@ static int _fastboot_parts_load_from_ptable(void)
 				/ dev_desc->blksz;
 #else
 	ptable[PTN_BOOTLOADER_INDEX].length =
-				 boot_loader_psize / dev_desc->blksz;
+				 (boot_loader_psize - bootloader_mmc_offset()) / dev_desc->blksz;
 #endif
 	ptable[PTN_BOOTLOADER_INDEX].partition_id = boot_partition;
 	ptable[PTN_BOOTLOADER_INDEX].flags = FASTBOOT_PTENTRY_FLAGS_UNERASEABLE;