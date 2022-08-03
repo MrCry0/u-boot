e923f3e8c34777f48b6a57898da5cfcbb799cefc
Michael Scott <mike@foundries.io>
2022-08-03 15:22:57 -0700

    ARM: imx: imx8mn-evk: define default dfu_alt_info for emmc updates
    Define dfu_alt_info to allow updating SPL/imx-boot and u-boot.itb via
    DFU (which is also used by EFI capsule updates).
    
    Signed-off-by: Michael Scott <mike@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

--- include/configs/imx8mn_evk.h
+++ include/configs/imx8mn_evk.h
@@ -38,6 +38,8 @@
 	BOOTENV \
 	"console=ttymxc1,115200\0" \
 	"boot_fit=no\0" \
+	"dfu_alt_info=mmc 2=imx-boot raw 0x0 0x300 mmcpart 1;" \
+		"u-boot-itb raw 0x300 0xD00 mmcpart 1\0" \
 	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	"bootm_size=0x10000000\0" \
 	"mmcpart=1\0" \
