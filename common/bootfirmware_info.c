MrCry0/u-boot

commit 57e1338895437ab4c4932d322131d56cd8f91b34

[FIO internal] common: introduce bootfirmware info
Introduce bootfirmware info functionality. Currently it's possible
to obtain version from DT and print it during boot:

U-Boot 2022.04+fio+gfd9b3d3ed4 (Mar 15 2023 - 20:28:36 +0000)
....
In:    serial
Out:   serial
Err:   serial
Boot firmware version: 147

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
(cherry picked from commit 8a9e798bc8f07414858fb5bd345d47fc56d0ecc9)

common/bootfirmware_info.c
@@ -1,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+#include <common.h>
+#include <dm.h>
+#include <init.h>
+#include <sysinfo.h>
+#include <asm/global_data.h>
+#include <linux/libfdt.h>
+#include <linux/compiler.h>
+
+#define COMPATIBLE "lmp,bootloader"
+
+int __weak get_boot_firmware_info(void)
+{
+	char *version;
+	int node;
+
+	node = fdt_node_offset_by_compatible(gd->fdt_blob, -1, COMPATIBLE);
+	if (node < 0) {
+		printf("Can't find node with compatible = \"" COMPATIBLE
+		       "\"\n");
+		return -FDT_ERR_NOTFOUND;
+	}
+
+	version = fdt_getprop(gd->fdt_blob, node, "bootfirmware-version", NULL);
+	if (version) {
+		printf("Boot firmware version: %s\n", version);
+		env_set("dt_bootfirmware_version", version);
+	} else {
+		return -FDT_ERR_NOTFOUND;
+	}
+
+	return 0;
+}

common/Kconfig
@@ -574,6 +574,13 @@ config DISPLAY_BOARDINFO
 	  when U-Boot starts up. The board function checkboard() is called
 	  to do this.
 
+config BOOTFIRMWARE_INFO
+	bool "Display information about the boot firmware"
+	depends on OF_CONTROL
+	default n
+	help
+	  Display information about the boot firmware (version etc).
+
 config DISPLAY_BOARDINFO_LATE
 	bool "Display information about the board during late start up"
 	help

common/Makefile
@@ -16,6 +16,7 @@ obj-y += board_f.o
 obj-y += board_r.o
 obj-$(CONFIG_DISPLAY_BOARDINFO) += board_info.o
 obj-$(CONFIG_DISPLAY_BOARDINFO_LATE) += board_info.o
+obj-$(CONFIG_BOOTFIRMWARE_INFO) += bootfirmware_info.o
 
 obj-$(CONFIG_FDT_SIMPLEFB) += fdt_simplefb.o
 obj-$(CONFIG_$(SPL_TPL_)OF_LIBFDT) += fdt_support.o

common/board_r.c
@@ -781,6 +781,9 @@ static init_fnc_t init_sequence_r[] = {
 	console_announce_r,
 	show_board_info,
 #endif
+#ifdef CONFIG_BOOTFIRMWARE_INFO
+	get_boot_firmware_info,
+#endif
 #ifdef CONFIG_ARCH_MISC_INIT
 	arch_misc_init,		/* miscellaneous arch-dependent init */
 #endif

include/init.h
@@ -299,6 +299,9 @@ int show_board_info(void);
 void get_reboot_reason(char *ret);
 #endif
 
+/* common/bootfirmware_info.c */
+int get_boot_firmware_info(void);
+
 /**
  * Get the uppermost pointer that is valid to access
  *

commit e3f7e4e65f9aefa2a18332f838f615eb8aaaf35a

[FIO tosquash] common: bootfirmware_info: fix building warnings
Fix using an undeclared function and miscasting of a const variable [1].

[1]
common/bootfirmware_info.c: In function ‘get_boot_firmware_info’:
common/bootfirmware_info.c:27:17: warning: assignment discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
   27 |         version = fdt_getprop(gd->fdt_blob, node, "bootfirmware-version", NULL);
      |                 ^
common/bootfirmware_info.c:30:17: warning: implicit declaration of function ‘env_set’ [-Wimplicit-function-declaration]
   30 |                 env_set("dt_bootfirmware_version", version);
      |                 ^~~~~~~

Fixes: 57e13388954 ("[FIO internal] common: introduce bootfirmware info")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

common/bootfirmware_info.c
@@ -2,6 +2,7 @@
 
 #include <common.h>
 #include <dm.h>
+#include <env.h>
 #include <init.h>
 #include <sysinfo.h>
 #include <asm/global_data.h>
@@ -12,7 +13,7 @@
 
 int __weak get_boot_firmware_info(void)
 {
-	char *version;
+	const char *version;
 	int node;
 	int ret = 0;
 